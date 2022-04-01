8dd658fe5308689ceb1956970596eda4235d06a8
Michael Chang <michaelchang.kuo@gmail.com>
2022-03-31 20:45:07 -0700

    can listen for input and change src

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
+/* eslint-disable no-console */
 /* global data */
 /* exported data */
+
+var imgTagLocation = document.querySelector('#img');
+console.log(imgTagLocation);
+var inputListening = document.querySelector('#photoURL');
+console.log(inputListening);
+
+inputListening.addEventListener('input', handleInput);
+
+function handleInput(event) {
+  // console.log(event.target.name, event.target.value);
+  imgTagLocation.setAttribute('src', event.target.value);
+  // console.log(imgTagLocation.getAttribute('src'));
+}
