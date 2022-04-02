abb6bb5a30b3135b7aa006d8abe69ead368cccbe
Michael Chang <michaelchang.kuo@gmail.com>
2022-04-02 16:56:17 -0700

    finished feature 1 user can create an entry

--- js/data.js
+++ js/data.js
@@ -6,3 +6,16 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var previousEntryJSON = localStorage.getItem('entry-local-storage');
+
+if (previousEntryJSON) {
+  data.entries = JSON.parse(previousEntryJSON);
+  data.nextEntryId = data.entries.length + 1;
+  // localStorage.removeItem('entry-local-storage');
+}
+
+window.addEventListener('beforeunload', function () {
+  var entryJSON = JSON.stringify(data.entries);
+  localStorage.setItem('entry-local-storage', entryJSON);
+});
--- js/main.js
+++ js/main.js
@@ -3,14 +3,24 @@
 /* exported data */
 
 var imgTagLocation = document.querySelector('#img');
-console.log(imgTagLocation);
-var inputListening = document.querySelector('#photoURL');
-console.log(inputListening);
+var formLocation = document.querySelector('#form');
 
-inputListening.addEventListener('input', handleInput);
+formLocation.addEventListener('submit', handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  var obj = {
+    name: formLocation.elements.title.value,
+    photoURL: formLocation.elements.photoURL.value,
+    notes: formLocation.elements.notes.value,
+    id: data.nextEntryId
+  };
+  data.nextEntryId++;
+  data.entries.push(obj);
+  formLocation.reset();
+}
 
-function handleInput(event) {
-  // console.log(event.target.name, event.target.value);
+var photoURLLocation = document.querySelector('#photoURL');
+photoURLLocation.addEventListener('input', handleURLInput);
+function handleURLInput(event) {
   imgTagLocation.setAttribute('src', event.target.value);
-  // console.log(imgTagLocation.getAttribute('src'));
 }
